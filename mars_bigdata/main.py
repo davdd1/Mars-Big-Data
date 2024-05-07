9069f98d30df0eeb9315e8814cd709480ef4d2b2
JesperMorais <jeppe.morris@hotmail.se>
2024-05-07 12:26:02 +0200

    added main and pages files

---
+++ mars_bigdata/main.py
@@ -0,0 +1,10 @@
+import requests
+import json
+import pandas as pd
+import streamlit as st
+
+#requsta från mars apin, spara i JSon fil
+
+#ta datan och hantera dvs visualisera i pandas dataframe
+
+#skapa en streamlit app som visar datan
