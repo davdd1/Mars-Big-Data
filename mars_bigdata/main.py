243b6ee19762180eb1eff9886b44e5f6c3a7cd4a
JesperMorais <jeppe.morris@hotmail.se>
2024-05-07 13:03:13 +0200

    added alot :)

--- mars_bigdata/main.py
+++ mars_bigdata/main.py
@@ -3,11 +3,19 @@ import json
 import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
-from dotenv import load_dotenv
-import os
 
+apikey = 'Bgt2DAbyTrtQuPO1XbqtAeTzfuVAjUiViACsdkge'
 #requsta från mars apin, spara i JSon fil
 
 #ta datan och hantera dvs visualisera i pandas dataframe
 
 #skapa en streamlit app som visar datan
+
+url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1001&api_key=DEMO_KEY'
+response = requests.get(url)
+data = response.json()
+
+with open('mars_data_weather.json', 'w') as f:
+    json.dump(data, f, indent=4)
+
+
