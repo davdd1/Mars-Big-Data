69b976344caf560c95af344fefaa5b153caacaa3
Jesper Morais <jeppe.morris@hotmail.se>
2024-05-08 16:08:29 +0200

    fixed updated

--- mars_bigdata/main.py
+++ mars_bigdata/main.py
@@ -9,21 +9,15 @@ def display_random_image(json_data):
     random_image = random.choice(json_data['photos'])
     img_url = random_image['img_src']
     
-#requsta från mars apin, spara i JSon fil
-#ta datan och hantera dvs visualisera i pandas dataframe
+    try:
+        response = requests.head(img_url, timeout=5)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        st.warning(f"Failed to load image: {e}")
+        return
+    
+    st.image(img_url, caption=random_image['camera'], width=500)
 
-#skapa en streamlit app som visar datan
-
-#url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1001&api_key=DEMO_KEY'
-#response = requests.get(url)
-#data = response.json()
-
-#with open('mars_data_weather.json', 'w') as f:
-    #json.dump(data, f, indent=4)
-
-# Initialize session state for the current image index
-# Initialize session state for the current image index
-# Set the page layout to wide to give more space for the image
 st.set_page_config(layout="wide")
 
 def set_background():
@@ -52,51 +46,6 @@ def set_background():
 
 set_background()
 
-
-# Initialize session state for the current image index
-if 'current_image_index' not in st.session_state:
-    st.session_state.current_image_index = 0
-
-# Load JSON data
-with open('mars_data_weather.json', 'r') as f:
-    json_data = json.load(f)
-
-# Get a list of photos
-photolinks = [photo['img_src'] for photo in json_data['photos']]
-
-
-# Dynamically adjust the number of columns based on the number of images
-num_images = len(photolinks)
-cols = st.columns(1)  # Adjust the number of columns to match the number of images
-
-# Button to show the next image
-if st.button('Next Image'):
-    # Select a random image from the list
-    st.session_state.current_image_index = random.randint(0, num_images - 1)
-
-# Display the current image
-if st.session_state.current_image_index < num_images:
-    
-    img_url = photolinks[st.session_state.current_image_index]
-    for url in json_data['photos']:
-        if url['img_src'] == img_url:
-            cameraname = url['camera']['full_name']
-            break
-        else:
-            cameraname = 'No camera name found'
-    
-    # Check if the image URL is valid and accessible
-    try:
-        response = requests.head(img_url, timeout=5)
-        response.raise_for_status()
-    except requests.exceptions.RequestException as e:
-        st.warning(f"Failed to load image: {e}")
-        return
-    
-    st.image(img_url, caption=random_image['camera'], width=500)
-
-
-
 sol = st.number_input("Enter a sol number:", min_value=0, max_value=1200, value=0)
 
 confirme = st.checkbox("I confirm that I want to see the photo of the sol number entered above.")
@@ -112,7 +61,7 @@ if sol > 0 and confirme:
         data = json.load(f)    
     display_random_image(data)  # Display a single random image
     
-    if st.button("Get new photo"):
-        with open ('mars_data_weather.json', 'r') as f:
-            data = json.load(f)
-        display_random_image(data)
+if st.button("Get new photo"):
+    with open ('mars_data_weather.json', 'r') as f:
+        data = json.load(f)
+    display_random_image(data)
