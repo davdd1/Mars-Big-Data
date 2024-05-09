c66dc3f41cdfa453a188be2d0ebe86104fd87da9
Jesper Morais <jeppe.morris@hotmail.se>
2024-05-09 19:07:56 +0200

    Fixed alot of shit

--- mars_bigdata/main.py
+++ mars_bigdata/main.py
@@ -5,62 +5,109 @@ import random
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-
+#define the function display image info
 apikey = 'Bgt2DAbyTrtQuPO1XbqtAeTzfuVAjUiViACsdkge'
+st.set_page_config(layout="wide", page_title="Mars Super Smart ML", page_icon="🚀", initial_sidebar_state="auto")
+col1, col2, col3 = st.columns(3)
+
+with col2:
+    def display_image_info(img_url):
+        with open ('mars_data_weather.json', 'r') as f:
+            data = json.load(f)
+            for photo in data['photos']: #för att veta vilken sol det är
+                if photo['img_src'] == img_url:
+                    rightSol = photo['sol']
+                    st.write(f"camera: {photo['camera']['full_name']}") #skriver ut kameran som användes
+                    break
+        #read from the csv file
+        #display the min and max temp for the sol    
+        df = pd.read_csv('mars-weather.csv')
+        for sol in df['sol']:
+            if sol == rightSol:
+                st.write(f"Min Temp: {df['min_temp'][sol]}", "°C")
+                st.write(f"Max Temp: {df['max_temp'][sol]}", "°C")
+                st.write(f"pressure: {df['pressure'][sol]}")
+                st.write(f"Earth Date: {df['terrestrial_date'][sol]}")
+                break    
+            
+        
+
+def send_image_request(sol): 
+    url = f'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol={sol}&api_key={apikey}'
+
+    try:
+        with open('mars_data_weather.json', 'r') as f:
+            data = json.load(f)
+    except FileNotFoundError:
+        st.warning("File not found")
+        return  # Exit the function if the file is not found
+
+    # Check if the request returns an empty response or no photos
+    newdata = requests.get(url)
+    if newdata.status_code!= 200:
+        st.warning("Failed to fetch data")
+        return  # Exit the function if the request fails
+
+    if newdata.json()['photos'] == []:
+        st.write("No data available for this sol")
+        return  # Exit the function if no data is available for the sol
+
+    # Update the JSON file only if new data is fetched
+    with open('mars_data_weather.json', 'w') as f:
+        json.dump(newdata.json(), f, indent=4)
 
 
-st.set_page_config(layout="wide")
-
-sol = st.number_input("Enter a sol number:", min_value=0, max_value=1200, value=0)
-
-df = pd.read_csv('mars-weather.csv')
+df = pd.read_csv('mars-weather.csv') #laddar in data från mars-weather.csv
 df['sol'] = df['sol'].astype(int) #skapar en ny kolumn med år
-df = df.dropna(subset=['min_temp', 'max_temp'])
+df = df.dropna(subset=['min_temp', 'max_temp']) #tar bort rader som inte är relevanta
 
 average_temps_by_sols = df.groupby('sol')[['min_temp', 'max_temp']].mean()
 
-sols = average_temps_by_sols.index
+with col1: #vänstra kolumnen
+    sol = st.number_input("Enter a sol number:", min_value=0, max_value=1200, value=0)
+    if sol:
+        send_image_request(sol)
 
-min_temp = average_temps_by_sols['min_temp'].mean()
-max_temp = average_temps_by_sols['max_temp'].mean()
-
-min_trend_line = np.polyfit(sols, average_temps_by_sols['min_temp'], 1)
-max_trend_line = np.polyfit(sols, average_temps_by_sols['max_temp'], 1)
-
-min_trend_func = np.poly1d(min_trend_line)
-max_trend_func = np.poly1d(max_trend_line)
-
-
-
-plt.figure(figsize=(10, 5))
-
-plt.plot(average_temps_by_sols.index, average_temps_by_sols['min_temp'], label='Min Temp', color='blue')
-plt.plot(average_temps_by_sols.index, average_temps_by_sols['max_temp'], label='Max Temp', color='red')
-
-plt.plot(average_temps_by_sols, min_trend_func(average_temps_by_sols), "black", label='Min Temp Trend')
-plt.plot(average_temps_by_sols, max_trend_func(average_temps_by_sols), "black", label='Max Temp Trend')
-
-plt.xlabel('Sol')
-plt.ylabel('Temperature')
-plt.title('Mars Weather')
-plt.legend()
-plt.show()
-
-st.title('Average Temperature Over the Years')
-st.pyplot(plt)
-
-def display_random_image(json_data):
-    random_image = random.choice(json_data['photos'])
-    img_url = random_image['img_src']
+    def display_random_image(json_data):
+        random_image = random.choice(json_data['photos'])
+        img_url = random_image['img_src']
+        
+        try:
+            response = requests.head(img_url, timeout=5)
+            response.raise_for_status()
+        except requests.exceptions.RequestException as e:
+            st.warning(f"Failed to load image: {e}")
+            return
+        
+        st.image(img_url, width=500)
+        display_image_info(img_url)
     
-    try:
-        response = requests.head(img_url, timeout=5)
-        response.raise_for_status()
-    except requests.exceptions.RequestException as e:
-        st.warning(f"Failed to load image: {e}")
-        return
+    if st.button("Get Image :sunglasses:"):
+        with open('mars_data_weather.json', 'r') as f:
+            data = json.load(f)
+        if (data['photos'] == []):
+            st.warning("No data available")
+        else:
+            display_random_image(data)
+        
     
-    st.image(img_url, caption=random_image['camera'], width=500)
+
+with col3: #högra kolumnen
+    fig, ax = plt.subplots(figsize=(10, 6))
+    min_temp = average_temps_by_sols['min_temp'].mean()
+    plt.plot(average_temps_by_sols.index, average_temps_by_sols['min_temp'], label='Min Temp', color='blue') #graf för min temp
+    plt.plot(average_temps_by_sols.index, average_temps_by_sols['max_temp'], label='Max Temp', color='red') #graf för max temp
+
+    plt.xlabel('Sol')
+    plt.ylabel('Temperature')
+    plt.title('Mars Weather')
+    plt.legend()
+    plt.show()
+
+# Display plot in Streamlit
+    st.title('Average Temperature Over the Years')
+    st.pyplot(plt)
+
 
 def set_background():
     #api_key = "DEMO_KEY"
@@ -87,29 +134,3 @@ def set_background():
     )
 set_background()
 
-
-
-
-if sol > 0 :
-    url = f'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol={sol}&api_key={apikey}'  
-    with open ('mars_data_weather.json', 'r') as f:
-        data = json.load(f)
-
-    if data['photos'] == []:
-        data = requests.get(url)
-        with open('mars_data_weather.json', 'w') as f:
-            json.dump(data.json(), f, indent=4)
-
-    elif sol != data['photos'][0]['sol']: #kollar om vi behöver hämta ny data eller ej
-        data = requests.get(url)
-        with open('mars_data_weather.json', 'w') as f:
-            json.dump(data.json(), f, indent=4)
-  
-    
-if st.button("Get Image :sunglasses:"):
-    with open('mars_data_weather.json', 'r') as f:
-        data = json.load(f)
-
-        display_random_image(data)
-         
-    
