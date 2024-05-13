464a4043e195d69f4d180dff14c0f751357e8733
davdd1 <149151516+davdd1@users.noreply.github.com>
2024-05-13 11:16:32 +0200

    Update background

--- mars_bigdata/main.py
+++ mars_bigdata/main.py
@@ -115,7 +115,7 @@ with col3: #högra kolumnen
 def set_background():
     #api_key = "DEMO_KEY"
     #date = "2024-05-04"
-    url = "https://apod.nasa.gov/apod/image/2405/three_ats_beletsky.jpg"
+    url = "https://images.pexels.com/photos/956981/milky-way-starry-sky-night-sky-star-956981.jpeg?cs=srgb&dl=pexels-felixmittermeier-956981.jpg&fm=jpg"
     #response = requests.get(url)
     #data = response.json()
 
